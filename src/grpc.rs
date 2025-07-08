2c696bd50dae919fa186f700900836fb6937cbd8
jh <jh.petrichor@gmail.com>
2025-07-08 18:33:41 +0800

    sol-trade-sdk

--- src/grpc.rs
+++ src/grpc.rs
@@ -1,12 +1,13 @@
-use futures_util::{SinkExt, StreamExt};
-use log::{debug, error};
-use solana_sdk::bs58;
+use futures_util::{SinkExt, StreamExt, sink::Fanout};
+use log::{debug, error, info};
+use solana_sdk::{account, bs58};
 use std::{collections::HashMap, sync::Arc, time::Duration};
 use tokio::sync::Mutex;
 use yellowstone_grpc_client::{ClientTlsConfig, GeyserGrpcClient};
 use yellowstone_grpc_proto::geyser::{
-    subscribe_update::UpdateOneof, CommitmentLevel, SubscribeRequest,
-    SubscribeRequestFilterTransactions, SubscribeRequestPing,
+    CommitmentLevel, SubscribeRequest, SubscribeRequestAccountsDataSlice,
+    SubscribeRequestFilterAccounts, SubscribeRequestFilterTransactions, SubscribeRequestPing,
+    subscribe_update::UpdateOneof,
 };
 
 use crate::handle::EventHandler;
@@ -50,6 +51,24 @@ impl YellowstoneGrpc {
                     failed: Some(false),
                     signature: None,
                     account_include: addrs,
+                    account_exclude: vec![],
+                    account_required: vec![],
+                },
+            )]),
+            commitment: Some(CommitmentLevel::Processed.into()),
+            ..Default::default()
+        };
+
+        let subscribe_request = SubscribeRequest {
+            transactions: HashMap::from([(
+                "".to_string(),
+                SubscribeRequestFilterTransactions {
+                    vote: Some(false),
+                    failed: Some(false),
+                    signature: None,
+                    account_include: vec![
+                        "BVdVonejnHwKAVFKx1YpQaBc8t225hFuzjns5ZMEq3Pp".to_string(),
+                    ],
                     account_exclude: vec![],
                     account_required: vec![],
                 },
@@ -103,5 +122,164 @@ impl YellowstoneGrpc {
         }
 
         Ok(())
+    }
+
+    pub async fn subscribe_account(
+        &self,
+        program_id: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let client = GeyserGrpcClient::build_from_shared(self.endpoint.clone())?
+            .tls_config(ClientTlsConfig::new().with_native_roots())?
+            .connect_timeout(Duration::from_secs(CONNECT_TIMEOUT))
+            .keep_alive_while_idle(true)
+            .timeout(Duration::from_secs(KEEP_ALIVE_TIMEOUT))
+            .connect()
+            .await?;
+
+        let client = Arc::new(Mutex::new(client));
+        // let addrs = vec![program_id];
+        let subscribe_request = SubscribeRequest {
+            accounts: HashMap::from([(
+                "balance".to_string(),
+                SubscribeRequestFilterAccounts {
+                    owner: vec!["BVdVonejnHwKAVFKx1YpQaBc8t225hFuzjns5ZMEq3Pp".to_string()],
+                    nonempty_txn_signature: Some(false),
+                    ..Default::default()
+                },
+            )]),
+            commitment: Some(CommitmentLevel::Processed.into()),
+            ..Default::default()
+        };
+
+        let subscribe_request = SubscribeRequest {
+            transactions: HashMap::from([(
+                "".to_string(),
+                SubscribeRequestFilterTransactions {
+                    vote: Some(false),
+                    failed: Some(false),
+                    signature: None,
+                    account_include: vec![
+                        "BVdVonejnHwKAVFKx1YpQaBc8t225hFuzjns5ZMEq3Pp".to_string(),
+                    ],
+                    account_exclude: vec![],
+                    account_required: vec![],
+                },
+            )]),
+            commitment: Some(CommitmentLevel::Processed.into()),
+            ..Default::default()
+        };
+
+        let (mut subscribe_tx, mut stream) = client
+            .lock()
+            .await
+            .subscribe_with_request(Some(subscribe_request))
+            .await?;
+
+        while let Some(message) = stream.next().await {
+            info!("{:?}", message);
+            match message {
+                Ok(msg) => match msg.update_oneof {
+                    Some(UpdateOneof::Account(account)) => {
+                        info!("{:?}", account);
+                    }
+                    Some(UpdateOneof::Transaction(ts)) => {
+                        info!("{:?}", ts);
+                    }
+                    Some(UpdateOneof::Ping(ping)) => {
+                        info!("{:?}", ping);
+                    }
+                    x => {
+                        info!("{:?}", x)
+                    }
+                },
+                Err(e) => {
+                    error!("Error: {:?}", e);
+                    break;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn subscribe_price(
+        &self,
+        program_id: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let client = GeyserGrpcClient::build_from_shared(self.endpoint.clone())?
+            .tls_config(ClientTlsConfig::new().with_native_roots())?
+            .connect_timeout(Duration::from_secs(CONNECT_TIMEOUT))
+            .keep_alive_while_idle(true)
+            .timeout(Duration::from_secs(KEEP_ALIVE_TIMEOUT))
+            .connect()
+            .await?;
+
+        let client = Arc::new(Mutex::new(client));
+        let addrs = vec![program_id];
+        let subscribe_request = SubscribeRequest {
+            transactions: HashMap::from([(
+                "client".to_string(),
+                SubscribeRequestFilterTransactions {
+                    vote: Some(false),
+                    failed: Some(false),
+                    signature: None,
+                    account_include: addrs,
+                    account_exclude: vec![],
+                    account_required: vec![],
+                },
+            )]),
+            commitment: Some(CommitmentLevel::Processed.into()),
+            ..Default::default()
+        };
+
+        let (mut subscribe_tx, mut stream) = client
+            .lock()
+            .await
+            .subscribe_with_request(Some(subscribe_request))
+            .await?;
+
+        while let Some(message) = stream.next().await {
+            match message {
+                Ok(msg) => match msg.update_oneof {
+                    Some(UpdateOneof::Account(account)) => {
+                        info!("{:?}", account);
+                    }
+                    _ => {}
+                },
+                Err(e) => {
+                    error!("Error: {:?}", e);
+                    break;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn request_price(mint: Vec<String>, owner: Vec<String>) -> SubscribeRequest {
+    SubscribeRequest {
+        accounts: HashMap::from([(
+            "price".to_string(),
+            SubscribeRequestFilterAccounts {
+                account: mint,
+                owner: owner,
+                ..Default::default()
+            },
+        )]),
+        accounts_data_slice: vec![SubscribeRequestAccountsDataSlice {
+            offset: 253,
+            length: 16,
+        }],
+        commitment: Some(CommitmentLevel::Processed.into()),
+        ..Default::default()
+    }
+}
+
+fn request_parser() -> SubscribeRequest {
+    SubscribeRequest {
+
+
+        ..Default::default()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use log::debug;
 use crate::grpc::YellowstoneGrpc;
 
+mod account;
 mod grpc;
 mod handle;
 mod model;
-mod account;
+mod common;
 
-const PROGRAM_ID1: &str = "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P";
-const PROGRAM_ID2: &str = "pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA";
-const PROGRAM_ID3: &str = "CebN5WGQ4jvEPvsVU4EoHEpgzq1VV7AbicfhtW4xC9iM";
+#[allow(unused)]
+// mod test;
+
+// const PROGRAM_ID1: &str = "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P";
+// const PROGRAM_ID2: &str = "pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA";
+const PROGRAM_ID3: &str = "BVdVonejnHwKAVFKx1YpQaBc8t225hFuzjns5ZMEq3Pp";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // dotenv::dotenv().ok();
+    // pretty_env_logger::init();
+
+    // let url = std::env::var("YELLOWSTONE_GRPC_URL").expect("YELLOWSTONE_GRPC_UTL must be set");
+
+    // test(url).await
     dotenv::dotenv().ok();
     pretty_env_logger::init();
 
@@ -19,8 +28,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let mut client = YellowstoneGrpc::new(url, None).build_client().await?;
     let client = YellowstoneGrpc::new(url, None);
 
-    debug!("Starting subscription for Pump: {}", PROGRAM_ID1);
-    debug!("Starting subscription for PumpAmm: {}", PROGRAM_ID2);
+    // debug!("Starting subscription for Pump: {}", PROGRAM_ID1);
+    // debug!("Starting subscription for PumpAmm: {}", PROGRAM_ID2);
 
     // 创建一个包含两个程序 ID 的向量
     let program_ids = vec![PROGRAM_ID3];
@@ -29,7 +38,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for program_id in program_ids {
         let client = client.clone();
         tokio::spawn(async move {
-            if let Err(e) = client.subscribe(program_id.to_string()).await {
+            if let Err(e) = client.subscribe_account(program_id.to_string()).await {
                 log::error!("Error subscribing to program {}: {:?}", program_id, e);
             }
         });
